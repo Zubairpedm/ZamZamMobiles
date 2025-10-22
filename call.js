455f9094635456eb51ab4f79f02ec9a95e398ae2
Zubairpedm <zubairpedm@gmail.com>
2025-10-23 03:15:07 +0530

    Update phone number for call initiation

--- call.js
+++ call.js
@@ -1,5 +1,5 @@
 // Replace with your real contact number
-const phoneNumber = "+919182679753";
+const phoneNumber = "+919177237342";
 
 function initiateCall() {
   window.location.href = `tel:${phoneNumber}`;
